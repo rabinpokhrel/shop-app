26940717647aca596bb9b06a8abe15aa6254151f
unknown <pokhrelrabin12@gmail.com>
2019-11-06 15:15:02 +0545

    first commit

---
+++ shop-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './homepage.component';
+import './App.css';
+import HomePage from './homepage.component';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+   <HomePage/>
+    );
+  }
+}
+ 
+
+
+export default App;
